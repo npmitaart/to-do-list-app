c7f46b4ad41b0310109e2f1a7cd9b14612ec3a49
Giodio Mitaart <mitaartmitaart@gmail.com>
2020-12-08 13:28:44 +0700

    connect to db

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+// connecting to database
+const dotenv = require('dotenv');
+dotenv.config();
 
 app.use('/static', express.static('public'));
 
 // function that will allow us to extract the data from the form 
 app.use(express.urlencoded({extended: true}));
+
+// connection to database
+mongoose.set('useFindAndModify', false);
+
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true}, () => {
+    console.log('Connected to db!');
+
+    app.listen(3000, () => console.log('Server running on port 3000'));
+});
 
 // embedded javascript. create .ejs files to use as the view template
 app.set('view engine', 'ejs');
@@ -18,5 +32,3 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
     console.log(req.body);
 });
-
-app.listen(3000, () => console.log('Server running on port 3000'));
